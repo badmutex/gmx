d1a291cb4c481dfdc3d1c96788b531456fd75742
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-09-30 00:49:43 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='gmx',
+      version='1.0.0',
+      py_modules= ['gmx'
+                   ],
+
+      author = "Badi' Abdul-Wahid",
+      author_email = 'abdulwahidc@gmail.com',
+      url = 'https://github.com/badi/gmx',
+      license = 'BSD',
+      description = 'Python wrappers for GROMACS executables'
+      )
